c5250e4da25e8ec8a777c6990b8cacd9752e5f2b
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-06-18 11:10:57 +0200

    Slow landing page nav transition

--- app/src/app/(landingpage)/LandingPage.jsx
+++ app/src/app/(landingpage)/LandingPage.jsx
@@ -172,7 +172,7 @@ export default function LandingPage({ data }) {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              transition={{ duration: 0.25, ease: "easeOut" }}
+              transition={{ duration: 1, ease: "easeOut" }}
             >
               <MainNav data={data} />
 
